151ec230b7b7582f55697133e5b9b2578449c59a
scien <scien@DESKTOP-A8IKF48>
2020-02-07 09:50:05 +0900

    입력받은 총 계단 갯수로 랜덤으로 각 계단 갯수 나누는 메서드

--- practice/src/practice/Ladder.java
+++ practice/src/practice/Ladder.java
@@ -11,32 +11,41 @@ public class Ladder {
 	
 	public static void main(String[] args) {
 		
-		int[][] ladder = new int[4][11];
+		// int[][] ladder = new int[5][12];
+		
+		// solve(n, answer);  // n은 계단수
 		
 		// 열 길이 ladder.length
 		// 행 길이 ladder[0].length
 		
+		// 열의 길이는 정해져 있음 (사다리 수) 5개 
+		int ladder = 5;
 		
+		// 입력받은 계단 수 
+		int n = 10;
+		
+		randomSteps(n, ladder);
 		
 	}
 
 	
-	// 정해진 행렬에 대하여 랜덤으로 계단 수를 생성하는 메서드
-	public static int[] randomSteps(int row, int column) {
-		int[] step = new int[column-1];
+	// 처음부터 배열의 크기를 정해놓으면 안됨
+	// 입력받은 계단 수를 랜덤으로 돌려서 3개의 값으로 나눈 뒤에
+	// 그 중 가장 큰 값과 (큰)옆 칸을 더하여 이차배열의 행의 값(y)이 되어서 이차배열을 선언
+	public static int[] randomSteps(int totalSteps, int ladders) {
+		int[] step = new int[ladders-1];
 		
-		int max = ((column-1)*(row-2)/2); // 최대로 만들 수 있는 계단의 수 13
-		int totalStep = ((int)(Math.random()*(max-(step.length-1)) )+step.length); // 3~13까지 출력
-
-		int temp = totalStep;
-		for(int i=step.length; i>0; i--) {
-			if(i==1) {
-				step[i-1] = totalStep - (step[i]+step[i+1]);
+		int sum = 0;
+		for(int i=step.length-1; i>=0; i--) {
+			if(i==0) step[i] = totalSteps - sum;
+			else {
+				step[i] = ((int)(Math.random()*(totalSteps-sum-i))+1);
+				sum += step[i];
 			}
-			else step[i-1] = ( (int)(Math.random()*(temp-i)) +1);
-			temp -= step[i-1];
 		}
 		return step;
 	}
 
+	
+	
 }
